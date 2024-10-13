d6f0707d1d3892e2f6fe806d35095db9bb9383a3
Nathan Wong <wkwong.nathan@gmail.com>
2024-10-13 21:39:41 +0800

    feat: add docker files for dev and build

--- src/app.ts
+++ src/app.ts
@@ -12,5 +12,5 @@ app.get('/', (req, res) => {
 const port = process.env.PORT || 3000;
 
 app.listen(port, () => {
-    console.log(`Server is running on port ${port}`);
+    console.log(`Server in ${process.env.NODE_ENV} is running on port ${port}`);
 });
