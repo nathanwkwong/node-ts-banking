5fefdaa3cac57defd7600c05a827602cae93321a
Nathan Wong <wkwong.nathan@gmail.com>
2024-10-26 14:18:52 +0800

    feat: add winston logger

---
+++ src/utils/logger.ts
@@ -0,0 +1,24 @@
+import winston from 'winston'
+
+export const logger = winston.createLogger({
+  level: 'info',
+  format: winston.format.json(),
+  transports: [
+    new winston.transports.Console(),
+    // write logs with level 'error' to `error.log` file
+    new winston.transports.File({ filename: './logs/error.log', level: 'error' }),
+    // write logs with level 'info' or higher to `combined.log` file
+    new winston.transports.File({ filename: './logs/combined.log' }),
+  ],
+})
+
+// log the console for non-production
+if (process.env.NODE_ENV !== 'production') {
+  logger.add(
+    new winston.transports.Console({
+      format: winston.format.simple(),
+    })
+  )
+}
+
+export const loggerInfo = logger.info
