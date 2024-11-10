e756e85d36215256a4d5764bcecaab4eb4b1f237
Nathan Wong <wkwong.nathan@gmail.com>
2024-11-10 14:25:11 +0800

    fix: adjust register password schema

--- src/schemas/user.schema.ts
+++ src/schemas/user.schema.ts
@@ -3,7 +3,13 @@ import { z } from 'zod'
 export const UserRegistrationSchema = z.object({
   username: z.string(),
   email: z.string().email(),
-  password: z.string().min(8),
+  password: z
+    .string()
+    .min(8, { message: 'Password must be at least 8 characters long' })
+    .regex(/[A-Z]/, { message: 'Password must contain at least one uppercase letter' })
+    .regex(/[a-z]/, { message: 'Password must contain at least one lowercase letter' })
+    .regex(/[0-9]/, { message: 'Password must contain at least one number' })
+    .regex(/[^A-Za-z0-9]/, { message: 'Password must contain at least one special character' }),
 })
 export type UserRegistrationDto = z.infer<typeof UserRegistrationSchema>
 
