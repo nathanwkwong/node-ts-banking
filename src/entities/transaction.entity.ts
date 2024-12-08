9e285de2024b09adf9b3d924de057536a01ab4c6
Nathan Wong <wkwong.nathan@gmail.com>
2024-12-08 20:50:03 +0800

    fix: alter transaction table

--- src/entities/transaction.entity.ts
+++ src/entities/transaction.entity.ts
@@ -1,15 +1,25 @@
-import { BaseEntity, Column, CreateDateColumn, Entity, ManyToOne, PrimaryGeneratedColumn } from 'typeorm'
+import { BaseEntity, Column, CreateDateColumn, Entity, JoinColumn, OneToOne, PrimaryGeneratedColumn } from 'typeorm'
 import { Account } from './account.entity'
+import { AccountCurrency } from '../constants/currency'
 
 @Entity()
 export class Transaction extends BaseEntity {
   @PrimaryGeneratedColumn('uuid')
   id: string
 
-  @ManyToOne(() => Account)
-  account: Account
+  @OneToOne(() => Account)
+  @JoinColumn({ name: 'receiverAccountId' })
+  receiver: Account
+
+  @OneToOne(() => Account)
+  @JoinColumn({ name: 'senderAccountId' })
+  sender: Account
+
+  @Column('enum', { enum: AccountCurrency, nullable: false })
+  currency: AccountCurrency
 
   @Column()
+  // e.g. bank, peer-to-peer, refund...
   transactionType: string
 
   @Column('decimal', { precision: 10, scale: 2 })
@@ -19,6 +29,7 @@ export class Transaction extends BaseEntity {
   description: string
 
   @Column()
+  // Pending/Started, Completed
   status: string
 
   @CreateDateColumn()
